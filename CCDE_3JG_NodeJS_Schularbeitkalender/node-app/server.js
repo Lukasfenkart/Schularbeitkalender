6f97a3c347aa690f33aca6bf16fcd9ea830ccf6c
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2022-06-23 14:16:08 +0200

    working

--- CCDE_3JG_NodeJS_Schularbeitkalender/node-app/server.js
+++ CCDE_3JG_NodeJS_Schularbeitkalender/node-app/server.js
@@ -32,38 +32,6 @@ app.post('/Sak', function (req,res){
     res.end(JSON.stringify(response));
 });
 
-//create database
-var url = "mongodb://mongoadmin:mypasswd@10.115.3.30:8017/SaK";
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-
-//create collection
-var url = "mongodb://mongoadmin:mypasswd@10.115.3.30:8017/";
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("SaK");
-  dbo.createCollection("Schularbeiten", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-});
-
-//insert in database
-MongoClient.connect(url, function(err, db, req) {
-  if (err) throw err;
-  var dbo = db.db("SaK");
-  var myobj = { Fach1: req.body.Fach1, Datum1: req.body.Datum1 };
-  dbo.collection("Schularbeiten").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
-
 app.listen(PORT, () => {
     console.log('Server connected at:', PORT);
 });
